bf7df251c1994b93675d8badd914f90cd74630ab
Sakshi <sakshinhoskeri@gmail.com>
2026-07-12 09:10:38 +0530

    Add brand filter dropdown to product listing view

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def product_list(request):
+    vendor_filter = request.GET.get('vendor', '')
+    
+    products = Product.objects.all().order_by('vendor', 'price')
+    
+    if vendor_filter:
+        products = products.filter(vendor=vendor_filter)
+    
+    vendors = Product.objects.values_list('vendor', flat=True).distinct().order_by('vendor')
+    
+    return render(request, 'tracking/product_list.html', {
+        'products': products,
+        'vendors': vendors,
+        'selected_vendor': vendor_filter,
+    })
