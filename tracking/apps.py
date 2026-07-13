cd03080709626aa76aab1b624e538e637354ce76
Sakshi <sakshinhoskeri@gmail.com>
2026-07-13 10:45:30 +0530

    Add email notification on user login via Django signals

--- tracking/apps.py
+++ tracking/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class TrackingConfig(AppConfig):
     name = 'tracking'
+
+    def ready(self):
+        import tracking.signals
---
+++ tracking/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.signals import user_logged_in
+from django.dispatch import receiver
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+@receiver(user_logged_in)
+def send_login_email(sender, user, request, **kwargs):
+    send_mail(
+        subject='D2C Pulse Tracker - Login Notification',
+        message=f'Hi {user.username},\n\nYou just logged into D2C Pulse Tracker.\n\nIf this wasn\'t you, please contact support.',
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[user.email] if user.email else [settings.EMAIL_HOST_USER],
+        fail_silently=True,
+    )
